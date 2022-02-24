d1a829b451de184755c2f4b48c8044faac81f822
jcmustin <jonathan.mustin@gmail.com>
2022-02-24 01:30:48 -0800

    Move timer to top, decrease opacity

--- src/components/Timer/styles.ts
+++ src/components/Timer/styles.ts
@@ -1,24 +1,24 @@
 import styled from '@emotion/styled'
-import { opacify } from 'polished'
+import { lighten, opacify } from 'polished'
 import { FONT_STYLE, PRIMARY_COLOR } from '../../constants/constants'
 
 export const Progress = styled.progress<{ isComplete?: boolean }>`
   position: fixed;
-  bottom: 0;
+  top: 0;
   left: 0;
   width: 100%;
-  height: 5.1vh;
-  max-height: 70px;
+  height: 9.5vh; // 5.1vh;
+  max-height: 220px;
   border-radius: 0;
   &::-webkit-progress-bar {
     background: transparent;
   }
   &::-webkit-progress-value {
     background: ${({ isComplete }) =>
-      opacify(-0.2, isComplete ? '#fff' : PRIMARY_COLOR)};
+      lighten(0.15, opacify(-0.6, isComplete ? '#fff' : PRIMARY_COLOR))};
     box-shadow: ${({ isComplete }) =>
-      isComplete ? '0 0 5px 6px #fffb, inset 0 0 25px 6px #fff' : 'none'};
-    transition: background-color 0.2s, box-shadow 0.6s;
+      isComplete ? '0 0 2px 3px #fffb, inset 0 0 80px 6px #fff' : 'none'};
+    transition: background-color 0.2s, box-shadow 0.3s;
     animation-play-state: running;
   }
 `
@@ -28,9 +28,9 @@ export const TaskTitle = styled.div`
   font-weight: bolder;
   ${FONT_STYLE};
   text-align: center;
-  opacity: 0.7;
+  opacity: 0.5;
   box-shadow: 0px 3px 0px #0003;
-  font-size: 4em;
+  font-size: 3.5em;
   position: fixed;
   left: 50%;
   background: #8884;
