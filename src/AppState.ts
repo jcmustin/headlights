84315c35c80ea9c0de89896ec16a300d0aa09d89
jcmustin <jonathan.mustin@gmail.com>
2022-03-19 21:12:29 -0700

    make activeTask computed property in appState

--- src/AppState.ts
+++ src/AppState.ts
@@ -13,27 +13,21 @@ export type AppState = {
 export const createAppState = (rawSchedule?: string): AppState => {
   let schedule = createSchedule(rawSchedule ? rawSchedule : '')
   let activeDay = new Date()
-  let activeTask = schedule.getActiveTask()
   return {
     get schedule() {
       return schedule
     },
     activeDay,
     get activeTask(): Task | undefined {
-      return activeTask
-    },
-    set activeTask(task: Task | undefined) {
-      activeTask = task
+      return schedule.getActiveTask()
     },
     completeActiveTask(status: Status = Status.Successful): void {
-      if (activeTask) {
-        activeTask.status = status
+      if (this.activeTask) {
+        this.activeTask.status = status
       }
-      activeTask = schedule.getActiveTask()
     },
     updateSchedule(rawSchedule: string) {
       schedule = createSchedule(rawSchedule)
-      activeTask = schedule.getActiveTask()
     },
   }
 }
