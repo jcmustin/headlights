c601fea52eb32f73ccd2531ce1c2f841f74677e6
jcmustin <jonathan.mustin@gmail.com>
2024-03-12 14:56:57 -0700

    Update task duration on completion

--- src/AppState.ts
+++ src/AppState.ts
@@ -52,16 +52,42 @@ export const createAppState = (rawSchedule?: string): AppState => {
       activeTask = schedule.getActiveTask()
     },
     completeActiveTask(status: Status = Status.Successful): void {
+      /**
+       * cachedActiveTask is the active task (i.e. the first task-like line in the schedule raw text
+       * which isn't complete) from the previous time we called this.updateActiveTask(). Unless something's
+       * amiss, it should be the one used to populate the timer view.
+       */
       const { activeTask: cachedActiveTask } = this
       this.updateActiveTask()
       const { activeTask: currentActiveTask } = this
-      if (!cachedActiveTask) return
-      if (!currentActiveTask?.is(cachedActiveTask)) {
+      /**
+       * if cachedActiveTask is undefined, it's not clear what we should update, so we exit.
+       */
+      if (!cachedActiveTask) {
+        console.error('no cached active task to update on task completion.')
+        return
+      }
+      /**
+       * if the cached active task isn't the current active task,
+       * (i.e. bc the user has modified the schedule mid-task,)
+       * make no guesses about what we should modify.
+       * Instead, insert a new entry for the cached active task
+       * before the current active task. can result in duplicates,
+       * but it doesn't destroy data.
+       */
+      else if (!currentActiveTask?.is(cachedActiveTask)) {
         schedule.insertBeforeActive(cachedActiveTask)
         cachedActiveTask.endTime = DateTime.now()
+        if (cachedActiveTask.startTime) {
+          cachedActiveTask.duration = Math.round(cachedActiveTask.endTime.diff(cachedActiveTask.startTime, 'minutes').minutes)
+        }
         cachedActiveTask.status = status
-      } else {
+      }
+      else {
         currentActiveTask.endTime = DateTime.now()
+        if (currentActiveTask.startTime) {
+          currentActiveTask.duration = Math.round(currentActiveTask.endTime.diff(currentActiveTask.startTime, 'minutes').minutes)
+        }
         currentActiveTask.status = status
       }
       this.updateActiveTask()
